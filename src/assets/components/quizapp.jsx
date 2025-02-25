f85214da0e3e0a2bc42ca74da10159f7b5ff0e72
neilalcovendas <158012667+neilalcovendas@users.noreply.github.com>
2025-02-25 11:56:04 +0800

    Add files via upload

---
+++ src/assets/components/quizapp.jsx
@@ -0,0 +1,34 @@
+function quizapp (props) {
+    return (
+        <div className="w-1/2 flex flex-col justify-center mx-auto">
+            <h1 className="font-bold text-xl text-center">Static Quiz App</h1>
+            <h3 className="font-semibold text-xl text-center">{props.projectname}</h3>
+
+            <div className="w-135 h-50 flex flex-col justify-center mx-auto mt-5 border border-transparent rounded-md shadow-2xl ">
+                <h3 className="ml-3.5 font-semibold">{props.quiz1}</h3>
+                <div className="w-125 border border-transparent rounded-md mx-auto bg-red-300 mb-0.5"> <p className="ml-2">{props.answer1}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-red-300 mb-0.5"> <p className="ml-2">{props.answer2}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-green-300 mb-0.5"> <p className="ml-2">{props.answer3}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-red-300 mb-0.5"> <p className="ml-2">{props.answer4}</p> </div>
+            </div>
+
+            <div className="w-135 h-50 flex flex-col justify-center mx-auto mt-5 border border-transparent rounded-md shadow-2xl">
+                <h3 className="ml-3.5 font-semibold">{props.quiz2}</h3>
+                <div className="w-125 border border-transparent rounded-md mx-auto bg-red-300 mb-0.5"> <p className="ml-2">{props.answer5}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-green-300 mb-0.5"> <p className="ml-2">{props.answer6}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-red-300 mb-0.5"> <p className="ml-2">{props.answer7}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-red-300 mb-0.5"> <p className="ml-2">{props.answer8}</p> </div>
+            </div>
+
+            <div className="w-135 h-50 flex flex-col justify-center mx-auto mt-5 border border-transparent rounded-md shadow-2xl">
+                <h3 className="ml-3.5 font-semibold">{props.quiz3}</h3>
+                <div className="w-125 border border-transparent rounded-md mx-auto bg-red-300 mb-0.5"> <p className="ml-2">{props.answer9}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-red-300 mb-0.5"> <p className="ml-2">{props.answer10}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-green-300 mb-0.5"> <p className="ml-2">{props.answer11}</p> </div>
+                <div className="w-125 border border-transparent rounded-md mx-auto  bg-red-300 mb-0.5"> <p className="ml-2">{props.answer12}</p> </div>
+            </div>
+            </div>
+    )
+}
+
+export default quizapp
